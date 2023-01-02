23f8e11ee34b999bc5b3bcd8b63e1c51a6c756ac
agrancaric <agrancaric@croz.net>
2023-01-02 21:19:43 +0100

    Simplify error logging

--- src/common/errors.rs
+++ src/common/errors.rs
@@ -27,12 +27,7 @@ impl Display for UserManagmenetError {
 
 impl ResponseError for UserManagmenetError {
     fn error_response(&self) -> HttpResponse {
-        match &self.error_type {
-            ErrorType::DieselError(error) => {
-                error!("Error occurred: {} with message: {}", error, self.message)
-            }
-            _ => error!("Error occurred with message: {}", self.message),
-        }
+        error!("Error occurred with message: {}", self.message);
 
         HttpResponse::InternalServerError()
             .json(json!({"error": self.message.to_string(), "timestamp": Local::now().to_string()}))
